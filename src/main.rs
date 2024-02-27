5cc94e217979d6d969425db2e7130985eb08a884
Sigmoidle <ben.jordanoubailey@btinternet.com>
2024-02-27 20:50:38 +0000

    cat ball wow!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 use comfy::*;
 
+const PAW_HEIGHT: f32 = 6.0;
+const PAW_WIDTH: f32 = 4.0;
+const PAW_SHAPE: Vec2 = Vec2 { x: PAW_WIDTH, y: PAW_HEIGHT };
+const PAW_ACCELERATION: f32 = 0.1;
+const PAW_FRICTION: f32 = -0.2;
+const BALL_RADIUS: f32 = 1.0;
+const BASE_BALL_SPEED: f32 = 0.1;
+
 comfy_game!("My Game!", GameState);
 
 // Define a global state object
 pub struct GameState {
-    pub x: f32,
-    pub y: f32,
+    pub left_paw_position: Vec2,
+    pub left_paw_velocity: Vec2,
+    pub left_paw_acceleration: Vec2,
+    pub right_paw_position: Vec2,
+    pub right_paw_velocity: Vec2,
+    pub right_paw_acceleration: Vec2,
+    pub ball_position: Vec2,
+    pub ball_movement_vec: Vec2,
+    pub score: u32,
+    pub best_score: u32,
     pub font: FontHandle,
 }
 
@@ -13,40 +29,140 @@ pub struct GameState {
 impl GameLoop for GameState {
     fn new(_c: &mut EngineState) -> Self {
         game_config_mut().bloom_enabled = true;
+        game_config_mut().resolution = ResolutionConfig::Logical(1000, 1000);
+        game_config_mut().tonemapping_enabled = true;
+
+        // Load assets
+        let textures = [
+            ("background", "background.png"),
+            ("paw_left", "paw_left.png"),
+            ("paw_right", "paw_right.png"),
+            ("ball", "ball.png"),
+        ];
+        pub static ASSET_DIR: include_dir::Dir<'_> =
+            include_dir::include_dir!("$CARGO_MANIFEST_DIR/assets");
+        init_asset_source(&ASSET_DIR, |path| {
+            format!(concat!(env!("CARGO_MANIFEST_DIR"), "/assets/{}"), path)
+        });
+
+        load_multiple_textures(
+            textures.iter().map(|(a, b)| (a.to_string(), b.to_string())).collect_vec(),
+        );
+
         Self {
-            x: 2.0,
-            y: 3.0,
+            left_paw_position: Vec2 { x: -PAW_WIDTH, y: 0.0 },
+            left_paw_velocity: Vec2::ZERO,
+            left_paw_acceleration: Vec2::ZERO,
+            right_paw_position: Vec2 { x: PAW_WIDTH, y: 0.0 },
+            right_paw_velocity: Vec2::ZERO,
+            right_paw_acceleration: Vec2::ZERO,
+            ball_position: Vec2 { x: 0.0, y: 0.0 },
+            ball_movement_vec: Vec2 { x: BASE_BALL_SPEED, y: BASE_BALL_SPEED },
+            score: 1,
+            best_score: 1,
             font: load_font_from_bytes(include_bytes!("../fonts/Orbitron-Bold.ttf")),
         }
     }
 
     fn update(&mut self, _c: &mut EngineContext) {
-        clear_background(TEAL);
+        clear_background(DARKPURPLE);
 
-        let text_position = screen_to_world(Vec2 { x: screen_width() / 2.0, y: 40.0 });
+        let screen_position = screen_to_world(Vec2 { x: screen_width(), y: screen_height() });
+        let score_position = screen_to_world(Vec2 { x: screen_width() / 2.0, y: 40.0 });
+        let best_score_position = screen_to_world(Vec2 { x: screen_width() / 2.0, y: 120.0 });
+        let ball_speed = BASE_BALL_SPEED + BASE_BALL_SPEED * (self.score as f32 / 40.0);
+
+        draw_sprite(texture_id("background"), Vec2::ZERO, WHITE, 1, screen_position * 2.0);
 
         draw_text_pro_experimental(
-            simple_styled_text(&format!("x: {} y: {}", self.x, self.y)),
-            text_position,
-            WHITE,
+            simple_styled_text(&format!("SCORE: {}", self.score)),
+            score_position,
+            BLACK,
             TextAlign::Center,
-            32.0,
+            60.0,
+            self.font,
+        );
+        draw_text_pro_experimental(
+            simple_styled_text(&format!("BEST SCORE: {}", self.best_score)),
+            best_score_position,
+            BLACK,
+            TextAlign::Center,
+            60.0,
             self.font,
         );
 
-        draw_circle(Vec2 { x: self.x, y: self.y }, 1.0, WHITE, 2);
-
-        if is_key_down(KeyCode::W) {
-            self.y += 0.2;
+        // Ball
+        if self.ball_position.x > (screen_position.x - BALL_RADIUS) {
+            self.ball_movement_vec.x = -ball_speed;
+            self.score += 1;
         }
-        if is_key_down(KeyCode::S) {
-            self.y -= 0.2;
+        if self.ball_position.x < -(screen_position.x) + BALL_RADIUS {
+            self.ball_movement_vec.x = ball_speed;
+            self.score += 1;
+        }
+        if self.ball_position.y > -(screen_position.y) - BALL_RADIUS {
+            self.ball_movement_vec.y = -ball_speed;
+            self.score += 1;
+        }
+        // Ball - Check for collision with paws
+        if rect_contains(self.right_paw_position, PAW_SHAPE, self.ball_position) {
+            self.ball_movement_vec.y = ball_speed;
+            self.score += 1;
+        }
+        if rect_contains(self.left_paw_position, PAW_SHAPE, self.ball_position) {
+            self.ball_movement_vec.y = ball_speed;
+            self.score += 1;
+        }
+        // Ball - Check end-game
+        if self.ball_position.y < screen_position.y - BALL_RADIUS {
+            self.ball_position = Vec2::ZERO;
+            self.score = 1
+        }
+        self.ball_position += self.ball_movement_vec;
+        if self.score > self.best_score {
+            self.best_score = self.score
+        }
+        draw_sprite(texture_id("ball"), self.ball_position, WHITE, 6, Vec2::ONE * 2.5);
+
+        // Paws
+        self.right_paw_acceleration = Vec2::ZERO;
+        self.left_paw_acceleration = Vec2::ZERO;
+        if is_key_down(KeyCode::Left) {
+            self.right_paw_acceleration.x = -PAW_ACCELERATION;
+        }
+        if is_key_down(KeyCode::Right) {
+            self.right_paw_acceleration.x = PAW_ACCELERATION;
         }
         if is_key_down(KeyCode::A) {
-            self.x -= 0.2;
+            self.left_paw_acceleration.x = -PAW_ACCELERATION;
         }
         if is_key_down(KeyCode::D) {
-            self.x += 0.2;
+            self.left_paw_acceleration.x = PAW_ACCELERATION;
         }
+
+        self.right_paw_acceleration.x += self.right_paw_velocity.x * PAW_FRICTION;
+        self.left_paw_acceleration.x += self.left_paw_velocity.x * PAW_FRICTION;
+        self.right_paw_velocity += self.right_paw_acceleration;
+        self.left_paw_velocity += self.left_paw_acceleration;
+        self.right_paw_position += self.right_paw_velocity + 0.5 * self.right_paw_acceleration;
+        self.left_paw_position += self.left_paw_velocity + 0.5 * self.left_paw_acceleration;
+
+        if self.right_paw_position.x > screen_position.x - (PAW_WIDTH / 2.0) {
+            self.right_paw_position.x = screen_position.x - (PAW_WIDTH / 2.0)
+        }
+        if self.right_paw_position.x < (PAW_WIDTH / 2.0) {
+            self.right_paw_position.x = PAW_WIDTH / 2.0
+        }
+        if self.left_paw_position.x < -screen_position.x + (PAW_WIDTH / 2.0) {
+            self.left_paw_position.x = -screen_position.x + (PAW_WIDTH / 2.0)
+        }
+        if self.left_paw_position.x > -(PAW_WIDTH / 2.0) {
+            self.left_paw_position.x = -(PAW_WIDTH / 2.0)
+        }
+        self.right_paw_position.y = screen_position.y + (PAW_HEIGHT / 2.0);
+        self.left_paw_position.y = screen_position.y + (PAW_HEIGHT / 2.0);
+
+        draw_sprite(texture_id("paw_right"), self.right_paw_position, WHITE, 5, PAW_SHAPE);
+        draw_sprite(texture_id("paw_left"), self.left_paw_position, WHITE, 5, PAW_SHAPE);
     }
 }
